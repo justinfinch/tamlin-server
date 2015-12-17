0847af89232fafa742aaf554eb8d6bc1618053c1
jfennell03 <jfennell@ourrecords.com>
2015-12-17 16:59:59 -0600

    Shop Floor Main: Added View & Module

---
+++ src/Tamlin.MCServer.Web/Modules/ShopFloorModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCServer.Core.Logging;
+using Nancy;
+using Nancy.Security;
+
+namespace Tamlin.MCServer.Web.Modules
+{
+    public class ShopFloorModule : NancyModule
+    {
+        public ShopFloorModule(ILogger logger)
+        {
+            this.RequiresAuthentication();
+
+            Get["/ShopFloor"] = parameters =>
+            {
+                logger.Info("You have loaded Shop Floor.");
+
+                var user = this.Context.CurrentUser;
+                dynamic test = new ExpandoObject();
+                test.Name = user.UserName;
+                return View["index", test];
+            };
+        }
+    }
+}
